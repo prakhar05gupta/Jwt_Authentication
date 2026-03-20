4d4ecd461228c2e3d5bff776d8f08850e0ed488e
Prakhar Gupta <123754352+prakhar05gupta@users.noreply.github.com>
2026-03-21 00:21:53 +0530

    Refactor CORS configuration to use origin patterns

--- user_service/src/main/java/com/project_management_system/user_service/config/CorsConfig.java
+++ user_service/src/main/java/com/project_management_system/user_service/config/CorsConfig.java
@@ -15,42 +15,23 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        //Allow specific origins (change for production)
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",   // React default
-                "http://localhost:4200",   // Angular default
-                "http://localhost:8080",   // API Gateway
-                "http://localhost:5500",   // VS Code Live Server
-                "http://127.0.0.1:5500",    // VS Code Live Server alternate
-            "https://prakhar05gupta.github.io"
-        ));
-        //Allow specific HTTP methods
+        configuration.setAllowedOriginPatterns(List.of("*"));
+
         configuration.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
-        //Allow specific headers
+
         configuration.setAllowedHeaders(Arrays.asList(
-                "Authorization",
-                "Content-Type",
-                "Accept"
+                "Authorization", "Content-Type", "Accept"
         ));
 
-        //Allow credentials
         configuration.setAllowCredentials(true);
-
-        //Expose headers
         configuration.setExposedHeaders(List.of("Authorization"));
-
-        // Max age
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",configuration);
+        source.registerCorsConfiguration("/**", configuration);
 
-        return source ;
+        return source;
     }
 }
