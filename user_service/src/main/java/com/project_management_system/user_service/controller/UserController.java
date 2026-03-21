558af0f9fe617d2993fbd826f731eaff29f75c05
Prakhar Gupta <123754352+prakhar05gupta@users.noreply.github.com>
2026-03-21 12:21:46 +0530

    Add CrossOrigin annotation to UserController

--- user_service/src/main/java/com/project_management_system/user_service/controller/UserController.java
+++ user_service/src/main/java/com/project_management_system/user_service/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
